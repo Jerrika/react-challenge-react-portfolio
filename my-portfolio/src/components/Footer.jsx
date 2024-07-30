2211bf0890907c049ee844110cc10671045973e0
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-07-30 18:20:20 -0500

    fixed linkedin link and profile pic.

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <p>Connect with me:</p>
       <a href="https://github.com/Jerrika/" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <a href="https://www.linkedin.com/in/yourusername/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/jerrika-gabriel?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">LinkedIn</a>
     </footer>
   );
 };
