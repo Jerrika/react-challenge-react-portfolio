5e19864eacb7a8f23ae70726e5c3417c913aa62e
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-07-30 18:49:01 -0500

    troubleshooting

--- my-portfolio/src/pages/AboutMe.jsx
+++ my-portfolio/src/pages/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
   return (
     <section>
       <h2>About Me</h2>
-      <img src="./src/assets/headshots-2020-Jerrika.JPG" alt="Jerrika Gabriel" width="500px" />
+      <img src="/src/assets/headshots-2020-Jerrika.JPG" alt="Jerrika Gabriel" width="500px" />
       <p>Hi there! I am Jerrika Gabriel, a motivated and enthusiastic full-stack developer who recently completed the Full-Stack Developer Bootcamp at Southern Methodist University. This comprehensive program provided me with a solid foundation in both front-end and back-end development, preparing me to create innovative and efficient web applications.</p>
     </section>
   );
